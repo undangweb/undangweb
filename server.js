bf161dddb4d15fa265d407ff2fbaa5cecb4f2a67
undangweb <141418595+undangweb@users.noreply.github.com>
2024-04-22 20:11:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const source = {
     keyMaps: "AIzaSyCpV55KNPo55TuxnDFd_dR3MD0luBrN1Gc",
     noimg: "https://satumomen.com/images/no-image.jpg",
     fotodefault:
-        "https://i.ibb.co/stGWV4L/mempelai1.jpg;https://i.ibb.co/TkwGXxF/mempelai2.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321256.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321808.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321295.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321269.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321310.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321359.jpg",
+        "https://i.ibb.co/S7CB7bf/mempelai1.jpg;https://i.ibb.co/5jjbr7V/mempelai2.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321256.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321808.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321295.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321269.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321310.jpg;https://assets.satumomen.com/images/galleries/32840-gallery-1681321359.jpg",
     idmusic: ["CRV3XQignAs", "ZEcqHA7dbwM"]
 };
